b7e4ec35dd519d1bb973aaeab8f9e1ca0123b7bd
Okan <101608474+OkanR1@users.noreply.github.com>
2022-03-24 17:58:26 +0700

    Add files via upload

---
+++ src/Components/karya.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Karya = () => {
+        return(
+            <div>
+                <h2>Ini Halaman Karya</h2>
+            </div>
+        )
+    }
+ export default Karya;
---
+++ src/Components/tentangsaya.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TentangSaya = () =>{
+    return(
+        <div>
+            <h2>Ini Halaman Tentang Tentang Saya</h2>
+        </div>
+    )
+}
+
+export default TentangSaya;
+ 
